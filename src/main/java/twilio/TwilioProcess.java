f05b329beb5e19d5c5a1101e7a2ac21c409b0503
Brandon <ncsbrandon@yahoo.com>
2020-12-09 17:02:56 -0500

    Create TwilioProcess.java

---
+++ src/main/java/twilio/TwilioProcess.java
@@ -0,0 +1,17 @@
+package twilio;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TwilioProcess {
+
+	private static Logger logger = LoggerFactory.getLogger(TwilioProcess.class.getSimpleName());
+
+	private static String VIXEN = "C:\\Program Files\\Vixen\\VixenApplication.exe";
+	private static String DIR = "C:\\Program Files\\Vixen\\";
+	
+	public TwilioProcess() {
+	}
+	
+	
+}
